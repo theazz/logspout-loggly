76280f9236c35a77663e4eb311f6691950506037
theazz <theaz.dm@gmail.com>
2017-10-09 13:26:57 +0300

    fix

--- build.sh
+++ build.sh
@@ -7,9 +7,10 @@ cd /go/src/github.com/gliderlabs/logspout
 export GOPATH=/go
 go get
 go build -ldflags "-X main.Version=$1" -o /bin/logspout
-apk del go git mercurial build-base
-rm -rf /go
-rm -rf /var/cache/apk/*
+
+#apk del go git mercurial build-base
+#rm -rf /go
+#rm -rf /var/cache/apk/*
 
 # backwards compatibility
 ln -fs /tmp/docker.sock /var/run/docker.sock
